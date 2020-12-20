874752f271ef86552bafccdefab95e6685fc9601
TaiyaKIE <46888320+book9222@users.noreply.github.com>
2020-12-20 22:28:44 +0700

    Add files via upload

---
+++ Java script/funtion/app.js
@@ -0,0 +1,11 @@
+function singSong() {
+    console.log("Yay Yay Yay");
+}
+
+function greet(person) {
+    console.log(`Hello, ${person}`)
+}
+function greet2(firstName, lastName) {
+    console.log(`My name is ${firstName} ${lastName[0].toUpperCase()}.`)
+}
+
