09509e5238ea51935866091914ffb38ce4c02c64
TaiyaKIE <46888320+book9222@users.noreply.github.com>
2020-12-21 12:07:34 +0700

    Add files via upload

--- Java script/funtion/app.js
+++ Java script/funtion/app.js
@@ -9,3 +9,12 @@ function greet2(firstName, lastName) {
     console.log(`My name is ${firstName} ${lastName[0].toUpperCase()}.`)
 }
 
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function splitWord(word) {
+    for(let i of word) {
+        console.log(i);
+    }
+}
