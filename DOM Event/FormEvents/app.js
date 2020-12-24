86562af9c15d596f5295b9b8d1b3e38dc25eba55
TaiyaKIE <46888320+book9222@users.noreply.github.com>
2020-12-24 11:45:01 +0700

    Add files via upload

--- DOM Event/FormEvents/app.js
+++ DOM Event/FormEvents/app.js
@@ -1,25 +1,19 @@
-
-
 const tweetForm = document.querySelector('#tweetForm');
 const tweetsContainer = document.querySelector('#tweets');
-tweetForm.addEventListener('submit', function (e) {
-    e.preventDefault();
 
-    // const usernameInput = document.querySelectorAll('input')[0];
+tweetForm.addEventListener("submit", function(evt) {
+    // const username = document.querySelectorAll('input')[0];
     // const tweetInput = document.querySelectorAll('input')[1];
-    const usernameInput = tweetForm.elements.username;
-    const tweetInput = tweetForm.elements.tweet;
-    addTweet(usernameInput.value, tweetInput.value)
-    usernameInput.value = '';
-    tweetInput.value = '';
-});
+    const user = tweetForm.elements.username.value;
+    const tweet = tweetForm.elements.tweet.value;
+    console.log(user, tweet);
+    evt.preventDefault();
 
-const addTweet = (username, tweet) => {
     const newTweet = document.createElement('li');
     const bTag = document.createElement('b');
-    bTag.append(username)
+    bTag.append(user);
     newTweet.append(bTag);
     newTweet.append(`- ${tweet}`)
+    
     tweetsContainer.append(newTweet);
-}
-
+})
